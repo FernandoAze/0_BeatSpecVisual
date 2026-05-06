c08c953bc3df5277474a90e39719ed07e45bf272
FernandoAze <up202308655@up.pt>
2026-05-06 16:50:53 +0100

    SCORE ALLIGNED V0

--- examples/NewSpectogramAlign.py
+++ examples/NewSpectogramAlign.py
@@ -17,17 +17,7 @@ WS = Warp_Score()
 audio_file = str(root_dir / "src/input_files/PARTITURAS_MEI/Chopin_op10_no3_p01.wav")
 svg_file = str(root_dir / "src/input_files/PARTITURAS_MEI/Chopin_Op10_3_1.mei-Chopin_op10_no3_p01-mei.maps.json.svg")
 maps_file = str(root_dir / "src/input_files/PARTITURAS_MEI/Chopin_op10_no3_p01-mei.maps.json")
-png_file = str(root_dir / "output/cropped_png.png")
+cropped_png_file = str(root_dir / "output/cropped_png.png")
 
-# WS.get_Timeline_Length(svg_file)
+WS.Allign_Score_and_PNG(cropped_png_file, svg_file, maps_file, audio_file)
 
-# WS.Combine_PlotPNG_wScore(png_file, svg_file, maps_file, png_file)
-
-# WS.extract_viewBox_dimensions(svg_file)
-# WS.get_timeline_from_notes(svg_file, maps_file)
-
-#WS.get_Timeline_Length(svg_file)
-
-#WS.get_first_and_last_note_positions(svg_file, maps_file)
-
-WS.get_timeline_adjusted(svg_file, maps_file)
