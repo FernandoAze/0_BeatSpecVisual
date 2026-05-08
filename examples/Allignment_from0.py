90358060f2a3b50a599b06a90af51053e00103a5
FernandoAze <up202308655@up.pt>
2026-05-08 17:04:59 +0100

    Just need to check the scaling fctor and of set of layers in the svg.

--- examples/Allignment_from0.py
+++ examples/Allignment_from0.py
@@ -25,7 +25,7 @@ from src.functions.Beat_Layers import (
 WS = Warp_Score()
 
 #Print states to console
-stuff_to_console=False
+stuff_to_console=True
 
 
 # Define input file paths 
@@ -35,52 +35,69 @@ the_maps_file = str(root_dir / input_parent_dir / "Chopin_op10_no3_p11-mei.maps.
 beat_output_path=str(root_dir / input_parent_dir / "CHOPIN_BEAT.npz")
 
 # 1st Creat Beat output file
-beatPrediction = Run_BeatThis(the_audio_file, beat_output_path)
+# beatPrediction = Run_BeatThis(the_audio_file, beat_output_path)
 
-#2nd Create Spectrogram PNG 
-spectogramConfig = {
-    "freq_window": (20, 2000),
-    "color_map": "viridis"
-}
-viz_spec = Visualizer()
-viz_spec.add_layer(Spectrogram(**spectogramConfig))
-viz_spec.load_all_layers(audio_path=the_audio_file)
-generated_png_file=viz_spec.TurnPlotIntoPNG("000_specTOGRAM.png", plot_size=(2010, 192), dpi=1200)
+# #2nd Create Spectrogram PNG 
+# spectogramConfig = {
+#     "freq_window": (20, 2000),
+#     "color_map": "viridis"
+# }
+# viz_spec = Visualizer()
+# viz_spec.add_layer(Spectrogram(**spectogramConfig))
+# viz_spec.load_all_layers(audio_path=the_audio_file)
+# generated_png_file=viz_spec.TurnPlotIntoPNG("000_specTOGRAM.png", plot_size=(2010, 192), dpi=1200)
 
 
-# 3rd Generate Layers and visualize, 
-viz=Visualizer()
-# viz.add_layer(Spectrogram(**spectogramConfig))
-viz.add_layer(BeatProbabilityLayer(color=(1, 0, 0))) # Red
-viz.add_layer(DownbeatProbabilityLayer(color=(0, 0, 1)))   # Blue
-viz.add_layer(BeatAccurateLayer(beat_color=(1, 1, 0), 
-                                downbeat_color=(0, 1, 0))) # Yellow beats, Green downbeats
-viz.add_layer(Onsets_Layer(onset_color=(1,1,1)))  # White onsets
-viz.load_all_layers( audio_path=the_audio_file,
-                    beat_file=beatPrediction, 
-                    # beat_file=beat_output_path,
-                     maps_file=the_maps_file)
+# # 3rd Generate the vecotor based Layers
+# viz=Visualizer()
+# viz.add_layer(BeatProbabilityLayer(color        =(1, 0, 0)))   # Red
+# viz.add_layer(DownbeatProbabilityLayer(color    =(0, 0, 1)))   # Blue
+# viz.add_layer(BeatAccurateLayer(beat_color      =(1, 1, 0),    # Yellow beats
+#                                 downbeat_color  =(0, 1, 0)))   # Green downbeats
+# viz.add_layer(Onsets_Layer(onset_color          =(1,1,1)))     # White onsets
 
-svg_Layer_Width = (WS.extract_ScoreSVG_dimensions(svg_score)[0])*0.03
-svg_Layer_Height = (WS.extract_ScoreSVG_dimensions(svg_score, print_output=True)[1])*0.03
+# viz.load_all_layers( audio_path    =the_audio_file,
+#                     # beat_file      =beatPrediction, 
+#                     beat_file      =beat_output_path,
+#                     maps_file      =the_maps_file)
 
-fig, ax = viz.draw()
-viz.TurnLayersIntoSVG(  filename    = str(root_dir / input_parent_dir / "LAYERS.svg"), 
-                        plot_size   = (svg_Layer_Width, svg_Layer_Height), 
-                        print_output= True)
+# svg_Layer_Width = (WS.extract_ScoreSVG_dimensions(svg_score)[0]) * 0.03
+# svg_Layer_Height = (WS.extract_ScoreSVG_dimensions(svg_score, print_output=True)[1]) * 0.03 
+# # The 0.03 is a scaling factor to match the svg dimension in this Example.
 
-# viz.show()
+# fig, ax = viz.draw() # This will draw layers as matplotlib objects.
 
-#4th Add allignment with score
+# Make the Plots into SVGs
+# malandro=viz.TurnLayersIntoSVG(  filename    = str(root_dir / input_parent_dir / "LAYERS.svg"), 
+#                         plot_size   = (svg_Layer_Width, svg_Layer_Height), 
+#                         print_output= True)
 
-gen_cropped_png_file = WS.crop_png(maps_file    = the_maps_file, 
-                                   png_file     = generated_png_file, 
-                                   audio_file   = the_audio_file,
-                                   print_output = stuff_to_console) 
-WS.Allign_Score_and_PNG(png_plot        = gen_cropped_png_file, 
-                        svg_image       = svg_score, 
-                        maps_json_file  = the_maps_file, 
-                        print_output    = stuff_to_console)
+# viz.show() #Displays the layers in a matplotlib window.
+
+
+
+
+#4th
+# gen_cropped_png_file = WS.crop_png(maps_file    = the_maps_file, 
+#                                    png_file     = generated_png_file, 
+#                                    audio_file   = the_audio_file,
+#                                    print_output = stuff_to_console) 
+# WS.Allign_Score_and_PNG(png_plot        = gen_cropped_png_file, 
+#                         svg_image       = svg_score, 
+#                         maps_json_file  = the_maps_file, 
+#                         print_output    = stuff_to_console)
+
+#5th Combine Layers
+# WS.extract_viewBox_dimensions(svg_score, print_output=True)
+WS.combine_AllignedScore_with_Layers(filename        = str(root_dir / input_parent_dir / "FINAL_SVG.svg"), 
+                                    alligned_svg    = str(root_dir / "output/composite.svg"), 
+                                    layers_svg      = str(root_dir / input_parent_dir / "LAYERS.svg"), 
+                                    scale_factor    = WS.scale_Factor(svg_score, print_output=True),
+                                    print_output    = stuff_to_console)
+
+
+
+
 
 # Layers and score are supposed generate a SVG with the diferent layers.
 # A simple UI can then be added just to enable/disable diferent layers: 
